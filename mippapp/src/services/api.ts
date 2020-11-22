bba11e66b34e1067222c76ff23a9e8f6b791b4dd
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2020-11-22 17:54:08 -0300

    Update

--- mippapp/src/services/api.ts
+++ mippapp/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://10.0.2.2:3333', //emulador
-  baseURL: 'http://192.168.0.16:3333',
+  baseURL: 'http://10.0.2.2:3333', //emulador
 });
 
 export default api;
